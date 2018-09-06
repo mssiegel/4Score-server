6f3ec1b68b23b10beabb2898707a7ef915ca2f13
Moshe Siegel <siegel.moshes@gmail.com>
2018-09-05 20:41:06 -0400

    refactored index.js code

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const app = express();
 const server = require('http').Server(app);
 
 const port = process.env.PORT || 4000;
-server.listen(port, () => {
-  console.log(`listening to requests on port ${port}`);
-});
+server.listen(port, () => console.log(`listening to requests on port ${port}`));
 
 //Socket setup
-//pingInterval sends ping every 10 seconds to make sure client still connected - necessary if client loses connection to internet
+//pingInterval sends ping every 10 seconds to make sure client is still connected - necessary if client loses connection to internet
 const io = socket(server, {pingInterval: 10000});
 
-//Enables CORS for socket.io for specified client urls with * port
+//Enables CORS for socket.io from specified client urls with * port
 //urls are separated by a space; * means any port
 io.origins('https://www.4scorechat.com:* http://localhost:*');
 
@@ -22,22 +20,20 @@ io.origins('https://www.4scorechat.com:* http://localhost:*');
 //Global variables to store socket data for all online users
 const chatQueue = []; // array of sockets waiting to chat
 const rooms = {}; // map socket.id => room
-const names = {}; // map socket.id => name
+const names = {}; // map socket.id => userName
 const allUsers = {}; // map socket.id => socket
 
 
 
 io.on('connection', socket => {
-  console.log('made socket connection', socket.id);
 
-  //Handle new person logging in
-  socket.on('new login', userName => {
-    console.log(`${userName} has entered the chatQueue`)
+  //Handle new person entering chatQueue
+  socket.on('enter chatQueue', userName => {
     names[socket.id] = userName;
     allUsers[socket.id] = socket;
     //check if someone is in queue
     findPeerForLoneSocket();
-  })
+  });
 
   //Handle new chat message
   socket.on('chat message', msg => {
@@ -48,7 +44,7 @@ io.on('connection', socket => {
   //Handle someone is typing a new chat message
   socket.on('typing', userName => {
     const room = rooms[socket.id];
-    socket.to(room).emit('typing', userName)
+    socket.to(room).emit('typing', userName);
   });
 
   //Handle client clicked endChatBtn
@@ -81,16 +77,18 @@ io.on('connection', socket => {
      rooms[peer.id] = room;
      rooms[socket.id] = room;
      // exchange names between the two of them and start the chat
-     console.log("people have been matched!!");
      peer.emit('chat start', names[socket.id]);
      socket.emit('chat start', names[peer.id]);
     }
-    else chatQueue.unshift(socket); // queue is empty, add our lone socket
+    // else queue is empty, add our lone socket
+    else chatQueue.unshift(socket);
   }
 
   function endChat(){
     const room = rooms[socket.id];
-    if(room){
+    //room may not exist if client lost internet during a chat and later reconnected as a brand new socket
+    //upon connecting the new socket will emit 'chat end' yet will obviously not have a room
+    if(room) {
       socket.to(room).emit('chat end');
       const socketAndPeerIDs = room.split('#');
       socketAndPeerIDs.forEach(socketID => {
