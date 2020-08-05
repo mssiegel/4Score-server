5c78ad438483b43b04a83c3db24c971e59668911
Moshe Siegel <siegel.moshes@gmail.com>
2020-08-05 10:27:49 -0400

    Added username to Pushover notification messages

--- socketIOSetup/notifications.js
+++ socketIOSetup/notifications.js
@@ -1,19 +1,19 @@
-var Push = require('pushover-notifications')
+const Push = require('pushover-notifications')
 
-var p = new Push({
+const p = new Push({
   user: process.env.PUSHOVER_USER,
   token: process.env.PUSHOVER_TOKEN,
 })
 
-var msg = {
-  // These values correspond to the parameters detailed on https://pushover.net/api
-  // 'message' is required. All other values are optional.
-  message: 'Someone entered chat queue',
-  url: 'https://www.4scorechat.com/chatroom/',
-  url_title: 'Visit chatroom',
-}
+function notifyAdmin(userName) {
+  const msg = {
+    // These values correspond to the parameters detailed on https://pushover.net/api
+    // 'message' is required. All other values are optional.
+    message: `"${userName}" entered chat queue`,
+    url: 'https://www.4scorechat.com/chatroom/',
+    url_title: 'Visit chatroom',
+  }
 
-function notifyAdmin() {
   p.send(msg, function (err, result) {
     if (err) throw err
 
